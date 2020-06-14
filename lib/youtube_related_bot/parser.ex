1505b964ccd4b72b46c5a6d6583de515740d429a
Ironjanowar <ironjanowar@gmail.com>
2020-06-14 23:23:26 +0200

    Improve regex (took it from the web :D)

--- lib/youtube_related_bot/parser.ex
+++ lib/youtube_related_bot/parser.ex
@@ -2,13 +2,16 @@ defmodule YoutubeRelatedBot.Parser do
   def extract_urls(text) do
     text
     |> String.split(~r/(\s|\n)/, trim: true)
-    |> Enum.filter(&String.match?(&1, ~r/^(http|https):\/\/(www\.|)youtube.com/))
+    |> Enum.filter(&String.match?(&1, ~r/^(http|https):\/\/(www\.|)(youtube.com|youtu.be)/))
   end
 
   def get_video_id(url) do
-    case Regex.run(~r/v=([^&]*)/, url) do
-      [_full_match, video_id | _] -> {:ok, video_id}
-      _ -> :no_match
+    case Regex.run(
+           ~r/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/,
+           url
+         ) do
+      nil -> :no_match
+      matches -> {:ok, List.last(matches)}
     end
   end
 end
